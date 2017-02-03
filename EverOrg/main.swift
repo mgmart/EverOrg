d3c7ecfe72200f63d7fcc64125b9b8b5e08c051d
Mario Martelli <cocoa@schnuddelhuddel.de>
2017-02-03 19:00:10 +0100

    Initial Commit

---
+++ EverOrg/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  EverOrg
+//
+//  Created by Mario Martelli on 03.02.17.
+//  Copyright © 2017 Schnuddel Huddel. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
