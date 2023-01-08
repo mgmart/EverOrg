24c484cac64e3737e83e6bee3670f675ef49101e
Mario Martelli <cocoa@schnuddelhuddel.de>
2023-01-08 09:54:56 +0100

    Readme is updated

---
+++ build.sh
@@ -0,0 +1,28 @@
+TRGT=everorg-0.3
+
+# Windows Intel
+GOOS=windows GOARCH=amd64 go build -o everorg.exe
+tar czf $TRGT-windows-amd64.tgz everorg.exe
+
+# Windows Intel 386
+GOOS=windows GOARCH=386 go build -o everorg.exe
+tar czf $TRGT-windows-386.tgz everorg.exe
+
+# Linux Intel
+GOOS=linux GOARCH=amd64 go build -o everorg
+tar czf $TRGT-windows-amd64.tgz everorg
+
+# Linux ARM
+GOOS=linux GOARCH=arm go build -o everorg
+tar czf $TRGT-windows-arm.tgz everorg
+
+# MacOS Intel
+GOOS=darwin GOARCH=amd64 go build -o everorg
+tar czf $TRGT-darwin-amd64.tgz everorg
+
+# MacOS ARM 
+GOOS=darwin GOARCH=arm64 go build -o everorg
+tar czf $TRGT-darwin-arm64.tgz everorg
+
+
+
